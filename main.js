4aa5f254e599c76c683d868c3c9c32a3d08acf6e
chanhbeck <chanhbeck27@gmail.com>
2021-03-10 23:19:49 +0700

    Add 2 sectiom + fix UI

--- main.js
+++ main.js
@@ -1,49 +1,60 @@
 var products;
-
 $(document).ready(() => {
-    getProducts(products);
+  getProducts(products);
 })
 const getProducts = (item) => {
-    console.log('hehe');
-    let url = 'https://technow-4b3ab.firebaseio.com/.json'
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.onload = function () {
-        if (this.status == 200) {
-            item = JSON.parse(this.responseText).Products;
-            loadProductSection(item, 'CPU');
-            loadProductSection(item, 'Case');
-        }
+  console.log('hehe');
+  let url = 'https://technow-4b3ab.firebaseio.com/.json';
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onload = function () {
+    if (this.status == 200) {
+      item = JSON.parse(this.responseText).Products;
+      loadProductSection(item, 'CPU');
+      loadProductSection(item, 'Case');
+      loadProductSection(item, 'GamingChair');
+      loadProductSection(item, 'Headphone');
+      // loadProductSection(item, 'Keyboard');
+      // loadProductSection(item, 'Laptop');
+      // loadProductSection(item, 'Mainboard');
+      // loadProductSection(item, 'Monitor');
+      // loadProductSection(item, 'Mouse');
+      // loadProductSection(item, 'PSU');
+      // loadProductSection(item, 'RAM');
+      // loadProductSection(item, 'SSD');
+      // loadProductSection(item, 'Speaker');
+      // loadProductSection(item, 'VGA');
     }
-    xhr.send();
+  }
+  xhr.send();
 }
 
 const loadProductSection = (item, section) => {
-    let sectionObj = item[section];
-    console.log(sectionObj);
-
-    for (let i = 1; i <= 4; i++) {
-        let product = sectionObj[section + '0' + i];
-        let newData = `<div class="col-lg-3 col-md-6 col-xs-12">
-        <div class="product mt-3 mb-5">
-            <div class="card">
-                <img class="card-img-top" src="${product.avatarURL}" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title" style="height: 45px;">${product.name}</h5>
-                    <div class="rating">
-                    <span class="fa fa-star text-warning"></span>
-                    <span class="fa fa-star text-warning"></span>
-                    <span class="fa fa-star text-warning"></span>
-                    <span class="fa fa-star text-warning"></span>
-                    <span class="fa fa-star"></span>
-                    <span>(${product.sold})</span>
-                  </div>
-                    <p href="#" class="text-danger mb-0">${product.price} đ</p>
-                </div>
-            </div>
-        </div>`
-
-        let section_row = '.'+section + '-row';
-        $(section_row).append(newData);
-    }
+  let sectionObj = item[section];
+  if(section == 'Laptop') section = 'Lap';
+  if(section == 'Monitor') section = 'Mon';
+  console.log(sectionObj);
+  for (let i = 1; i <= 4; i++) {
+    let product = sectionObj[section + '0' + i];
+    let newData = `<div class="col-lg-3 col-md-6 col-xs-12 rounded">
+      <div class="product mt-3 mb-5">
+        <div class="card">
+        <img class="card-img-top" src="${product.avatarURL}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title rounded">${product.name}</h5>
+          <div class="rating">
+            <span class="fa fa-star text-warning"></span>
+            <span class="fa fa-star text-warning"></span>
+            <span class="fa fa-star text-warning"></span>
+            <span class="fa fa-star text-warning"></span>
+            <span class="fa fa-star"></span>
+            <span>(${product.sold})</span>
+          </div>
+          <p href="#" class="text-danger mb-0 price">${product.price.toLocaleString()}₫</p>
+        </div>
+      </div>
+    </div>`
+    let section_row = '.'+section + '-row';
+    $(section_row).append(newData);
+  }
 }
