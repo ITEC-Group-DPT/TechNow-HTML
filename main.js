58b14ff8a9e427b086bbea976159cfa14f68926c
chanhbeck <chanhbeck27@gmail.com>
2021-03-13 14:03:31 +0700

    Fix UI in card and bla bla bla
    Only Keyboard, Case, Monitor in homepage
    Some js & css to suit the change

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ const getProducts = (item) => {
   xhr.onload = function () {
     if (this.status == 200) {
       item = JSON.parse(this.responseText).Products;
-      loadProductSection(item, 'CPU');
+      //loadProductSection(item, 'CPU');
       loadProductSection(item, 'Case');
-      loadProductSection(item, 'GamingChair');
-      loadProductSection(item, 'Headphone');
+      //loadProductSection(item, 'GamingChair');
+      //loadProductSection(item, 'Headphone');
       loadProductSection(item, 'Keyboard');
       // loadProductSection(item, 'Laptop');
       // loadProductSection(item, 'Mainboard');
@@ -31,27 +31,12 @@ const getProducts = (item) => {
 
 const loadProductSection = (item, section) => {
   let sectionObj = item[section];
-<<<<<<< Updated upstream
-  if(section == 'Laptop') section = 'Lap';
-  if(section == 'Monitor') section = 'Mon';
-  console.log(sectionObj);
-  for (let i = 1; i <= 4; i++) {
-    let product = sectionObj[section + '0' + i];
-    let newData = `<div class="col-lg-3 col-md-6 col-xs-12 rounded">
-      <div class="product mt-3 mb-5">
-        <div class="card">
-=======
   console.log(sectionObj);
   for (let i = 10; i <= 17; i++) {
-    // let padding = '';
-    // if (i == 1) padding = 'pl-0'
-    // if (i == 4) padding = 'pr-0' ${padding}
-
     let product = sectionObj[section + i];
     let newData =
-      `<div class="col-lg-3 col-md-6 col-xs-12 my-3 mx-0  rounded">
-        <div class="card product w-100 h-100">
->>>>>>> Stashed changes
+      `<div class="col-lg-3 col-md-6 col-xs-12 my-3 mx-0 rounded">
+        <div class="card product">
         <img class="card-img-top" src="${product.avatarURL}" alt="Card image cap">
         <div class="card-body h-75">
           <h5 class="card-title rounded">${product.name}</h5>
