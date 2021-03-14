77a441eb578c1fccc8e05d0edad452033158128c
kurozemi <tranngminhdao@gmail.com>
2021-03-14 08:03:46 +0700

    merge products and header

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
 var products;
-
 $(document).ready(() => {
   getProducts(products);
 })
-
-
 const getProducts = (item) => {
+  console.log('hehe');
   let url = 'https://technow-4b3ab.firebaseio.com/.json';
   let xhr = new XMLHttpRequest();
   xhr.open("GET", url, true);
   xhr.onload = function () {
     if (this.status == 200) {
       item = JSON.parse(this.responseText).Products;
-      sortingSold(item);
-      loadProductSection(item, 'CPU');
+      //loadProductSection(item, 'CPU');
       loadProductSection(item, 'Case');
-      loadProductSection(item, 'GamingChair');
-      loadProductSection(item, 'Headphone');
-      // loadProductSection(item, 'Keyboard');
+      //loadProductSection(item, 'GamingChair');
+      //loadProductSection(item, 'Headphone');
+      loadProductSection(item, 'Keyboard');
       // loadProductSection(item, 'Laptop');
       // loadProductSection(item, 'Mainboard');
-      // loadProductSection(item, 'Monitor');
+      loadProductSection(item, 'Monitor');
       // loadProductSection(item, 'Mouse');
       // loadProductSection(item, 'PSU');
       // loadProductSection(item, 'RAM');
@@ -32,28 +29,17 @@ const getProducts = (item) => {
   xhr.send();
 }
 
-// slider
-
-//end slider
-
 const loadProductSection = (item, section) => {
   let sectionObj = item[section];
-  if (section == 'Laptop') section = 'Lap';
-  if (section == 'Monitor') section = 'Mon';
   console.log(sectionObj);
-  for (let i = 1; i <= 4; i++) {
-    let padding = '';
-    if (i == 1) padding = 'pl-0'
-    if (i == 4) padding = 'pr-0'
-
-    let product = sectionObj[section + '0' + i];
+  for (let i = 10; i <= 17; i++) {
+    let product = sectionObj[section + i];
     let newData =
-      `<div class="col-lg-3 col-md-6 col-xs-12 my-3 mx-0 ${padding} rounded">
-        <div class="card product w-100 h-100">
-        <img class="card-img-top" src="${product.avatarURL}" alt="Card image cap">
-        <div class="card-body">
+      `<div class="col-xl-3 col-lg-6 col-md-12 my-3 mx-0 rounded">
+        <div class="card product">
+        <a href="#" class="img-card"><img class="card-img-top" src="${product.avatarURL}" alt="Card image cap"></a>
+        <div class="card-body h-75">
           <h5 class="card-title rounded">${product.name}</h5>
-          <div class="bottom-price-star">
           <div class="rating">
             <span class="fa fa-star text-warning"></span>
             <span class="fa fa-star text-warning"></span>
@@ -63,93 +49,14 @@ const loadProductSection = (item, section) => {
             <span>(${product.sold})</span>
           </div>
           <p href="#" class="text-danger mb-0 price">${product.price.toLocaleString()}₫</p>
-          </div>
         </div>
+      </div>
     </div>`
-    let section_row = '.' + section + '-row';
+    let section_row = '.'+section + '-row';
     $(section_row).append(newData);
   }
 }
 
-function sortingSold(itemset) {
-  let list = [];
-  console.log("hello");
-  for (const catalog in itemset) {
-    for (const item in itemset[catalog]) {
-      if (Number.isInteger(itemset[catalog][item].sold)) {
-        list.push(itemset[catalog][item])
-      }
-      // sort theo từng catalog top rating 2 cái / catalog
-      // if (productlist.length == 0 || productlist.length == 1){
-      //   productlist.push(item[key][keyinkey]);
-      //   continue;
-      // }
-      // if(item[key][keyinkey].sold >= productlist[0].sold){
-      //   productlist.pop();
-      //   let prevsold = productlist.pop();
-      //   productlist.push(item[key][keyinkey])
-      //   productlist.push(prevsold);
-      // }
-    }
-  }
-  console.log(list);
-  list.sort(function (a, b) {
-    return b.sold - a.sold;
-  })
-  console.log(list);
-  let slider = document.querySelector(".my-slider")
-  for (let index = 0; index < 20; index++) {
-    let newData = `
-      <div class="product">
-        <div class="card product w-100 h-100">
-        <img class="card-img-top" src="${list[index].avatarURL}" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title rounded">${list[index].name}</h5>
-          <div class="bottom-price-star">
-          <div class="rating">
-            <span class="fa fa-star text-warning"></span>
-            <span class="fa fa-star text-warning"></span>
-            <span class="fa fa-star text-warning"></span>
-            <span class="fa fa-star text-warning"></span>
-            <span class="fa fa-star"></span>
-            <span>(${list[index].sold})</span>
-          </div>
-          <p href="#" class="text-danger mb-0 price">${list[index].price.toLocaleString()}₫</p>
-          </div>
-        </div>
-        </div>`
-    $('.my-slider').append(newData);
-  }
-  loadSlider();
-}
-function loadSlider() {
-  let slider = tns({
-    container: '.my-slider',
-    items: 1,
-    gutter: 15,
-    slideBy: 2,
-    autoplay: true,
-    controlsContainer: '#controls',
-    edgePadding: 10,
-    prevButton: '.previous',
-    nextButton: '.next',
-    autoplayButton: '.auto',
-    nav: false,
-    responsive: {
-      640: {
-        items: 2
-      },
-      1200: {
-        items: 3
-      },
-      1400: {
-        items: 4
-      }
-    },
-  });
-}
-getProducts(products);
-console.log(products);
 
 // UI
 let popUpNavItems = document.querySelector(".pop-up-items")
