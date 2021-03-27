a80d05584dbe8cccda42cb4607b3dff716a1f3f4
JustTory <johnnynathany@gmail.com>
2021-03-27 18:27:17 +0700

    Nav+footer contactus

--- pages/Contact/contact.js
+++ pages/Contact/contact.js
@@ -1,5 +1,14 @@
+let numberItemCart = document.querySelectorAll(".number-item-cart");
+let cartList = [];
+let cart = document.querySelector(".cart-list");
+
+
 $(document).ready(function () {
-    console.log('helo');
+    cartList = JSON.parse(localStorage.getItem("cartList"));
+    console.log("CART ON PAGE LOAD");
+    console.log(cartList);
+    updateNoItemInCart();
+
     $('.submit-form').submit(function (e) {
         e.preventDefault();
         $('.alert').removeClass('d-none');
@@ -9,3 +18,9 @@ $(document).ready(function () {
         }, 2000);
     });
 });
+
+function updateNoItemInCart() {
+    numberItemCart.forEach(number => {
+        number.innerText = getTotalItemsInCart();
+    });
+}
